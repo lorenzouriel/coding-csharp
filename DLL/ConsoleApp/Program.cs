cce3b59b94b12a8a454e0b6dce794272ee862da3
Lorenzo Uriel <lorenzo.uriel@geosiga.com.br>
2024-03-05 15:59:02 -0300

    An example of how to create a DLL file was added

---
+++ DLL/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DLL;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = MathService.Divide(20, 5);
+            Console.WriteLine(result);
+
+            Console.Read();
+        }
+    }
+}
